18a0a03611cc3a57d7ca4c12abb9591f2458db47
Roman <Melnikov_RV@list.ru>
2024-08-31 08:09:30 +0700

    Создаст все компоненты

--- src/general-presenter.js
+++ src/general-presenter.js
@@ -1,15 +1,40 @@
 import { render, RenderPosition } from './render';
+import FilterListView from './view/filter-list-view';
 import TripInfoView from './view/trip-info-view';
+import SortListView from './view/sort-list-view';
+import PointListView from './view/point-list-view';
+import PointItemView from './view/point-item-view';
+import MessageView from './view/message-view';
+import PointFormView from './view/point-form-view';
 
 export default class GeneralPresenter {
-
-  constructor({pageHeaderContainer, pageMainContainer}) {
+  constructor({ pageHeaderContainer, pageMainContainer }) {
     this.pageHeaderContainer = pageHeaderContainer;
     this.pageMainContainer = pageMainContainer;
   }
 
   init() {
-    const tripInfoContainer = this.pageHeaderContainer.querySelector('.trip-main');
+    const tripInfoContainer =
+      this.pageHeaderContainer.querySelector('.trip-main');
+    const filterListContainer = this.pageHeaderContainer.querySelector(
+      '.trip-controls__filters'
+    );
+    const contentContainer =
+      this.pageMainContainer.querySelector('.trip-events');
+    const pointListComponent = new PointListView();
+
     render(new TripInfoView(), tripInfoContainer, RenderPosition.AFTERBEGIN);
+    render(new FilterListView(), filterListContainer, RenderPosition.BEFOREEND);
+    render(new SortListView(), contentContainer, RenderPosition.BEFOREEND);
+    render(pointListComponent, contentContainer, RenderPosition.BEFOREEND);
+    render(new PointFormView, pointListComponent.getElement(), RenderPosition.BEFOREEND);
+    for (let i = 1; i < 4; i++) {
+      render(
+        new PointItemView(),
+        pointListComponent.getElement(),
+        RenderPosition.BEFOREEND
+      );
+    }
+    render(new MessageView(), contentContainer, RenderPosition.BEFOREEND);
   }
 }
